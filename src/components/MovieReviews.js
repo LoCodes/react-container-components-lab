8b8ca166ee1f187ac539dcebbd5c8ae30764d140
Loren Cipriano <loren25cipriano@gmail.com>
2021-08-17 09:05:00 -0700

    not finished

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,16 @@
 // Code MovieReviews Here
+// import Review from './Review'
+
+
+import React from 'react'
+
+const MovieReviews = ({reviews}) => {
+    return (
+        <div className="review-list">
+            { reviews.map(review => review) }
+            
+        </div>
+    )
+}
+
+export default MovieReviews
